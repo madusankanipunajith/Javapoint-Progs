4390de2d27baa1e23607d15e40d77eab0e145a2e
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-12-28 08:13:49 +0530

    Add advanced Q16

--- src/com/company/Q16.java
+++ src/com/company/Q16.java
@@ -10,6 +10,13 @@ public class Q16 {
         myMap.put(0, "zero");myMap.put(1, "one");myMap.put(2,"two");myMap.put(3, "three");
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
-        System.out.println(input);
+        String str = new String(String.valueOf(input)); int length = str.length();
+
+        while(length != 0){
+            int power = (int) Math.pow(10, length-1);
+            int result = input/power; input = input%power;
+            System.out.print(myMap.get(result)+" ");
+            length--;
+        }
     }
 }
